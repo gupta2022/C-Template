69a9eeffddb565ec718643ce96b606498291aa75
Gaurav Gupta <guptagaurav2022@gmail.com>
2021-05-02 14:58:16 +0530

    Vector and Map

--- Template.cpp
+++ Template.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
 
+//Vector 
+    vector <int> v;
+    
+    //size--returns size
+    vector.size()
+    
+    //insert
+    vector.push_back(value);
+
+//Maps
+    unordered_map<int, int> hash;
+
+    //find If the given key exists in unordered_map it returns an iterator to that element otherwise it returns the end of the map iterator.
+    hash.find(numberToFind)
+    //insert
+    hash[numbers[i]] = i;
+    // returns the end of the map iterator.
+    hash.end()
